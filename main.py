291fad390d3d0324e527efca4d97ed36ede3fa5a
dabowski <ukrytytomek@gmail.com>
2021-06-08 11:26:25 +0200

    Adds colorama to color input.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import colorama
 import feedparser
+from colorama import Fore, Style
 
 
 def save_urls(file_name):
@@ -8,11 +10,12 @@ def save_urls(file_name):
 
 
 def print_feed(feed):
-    print(feed['channel']['title'])
+    print(Fore.RED, feed['channel']['title'], Style.RESET_ALL, '\n')
     for item in feed['items']:
-        print(item['title'], item['link'], "\n")
+        print(Fore.YELLOW, Style.BRIGHT, item['title'], Style.RESET_ALL, item['link'], "\n")
 
 
+colorama.init()
 for url in save_urls('urls.txt'):
     print_feed(feedparser.parse(url))
     input("Press any key to move forward...")
