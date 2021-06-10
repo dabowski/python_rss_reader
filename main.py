6387f9e777f8f3841ae7cd855ce06877d2248596
dabowski <ukrytytomek@gmail.com>
2021-06-10 09:37:18 +0200

    Just prints RSS feeds to terminal

--- main.py
+++ main.py
@@ -9,33 +9,21 @@ def save_lines_to_array(file_name):
         return lines
 
 
-def save_line_to_file(line):
-    with open('cache.txt', 'a') as f:
-        f.write(line)
-        f.write('\n')
-
-
-def print_feed(feed, cache_array):
+def print_feed(feed):
     print(Fore.RED, feed['channel']['title'], Style.RESET_ALL, '\n')
     for item in feed['items']:
-        title_url = Fore.YELLOW + Style.BRIGHT + item['title'] + Style.RESET_ALL + item['link']
-        # TODO bug-fix so that program do not print element that are in the cache.txt
-        if title_url not in cache_array:
-            print(title_url)
-            save_line_to_file(title_url)
-        else:
-            pass
+        title_url = Fore.YELLOW + Style.BRIGHT + item['title'] + Style.RESET_ALL + " " + item['link']
+        print(title_url)
 
 
-def print_feeds(url_array, cache_array):
+def print_feeds(url_array):
     for url in url_array:
-        print_feed(feedparser.parse(url), cache_array)
+        print_feed(feedparser.parse(url))
         print("Press any key to move to continue...")
 
 
 if __name__ == "__main__":
     colorama.init()
-    cache = save_lines_to_array("cache.txt")
     urls = save_lines_to_array("urls.txt")
 
-    print_feeds(urls, cache)
+    print_feeds(urls)
